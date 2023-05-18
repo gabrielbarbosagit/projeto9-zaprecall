73d216e4628cd3f5c681f85fc6f32efff91f8bdc
gabriel barbosa <gabrielb_lima@hotmail.com>
2023-05-18 17:38:02 -0300

    teste github

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-<<<<<<< HEAD
-import './index.css'
-=======
->>>>>>> b4da29c (teste github)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-<<<<<<< HEAD
+
 )
-=======
-)
->>>>>>> b4da29c (teste github)
