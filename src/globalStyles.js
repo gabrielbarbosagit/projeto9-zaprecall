958406a90da0a706ba5a66872edfa08be8822a78
gabriel barbosa <gabrielb_lima@hotmail.com>
2023-05-20 14:34:28 -0300

    vite config

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -1,8 +1,7 @@
 import { createGlobalStyle } from "styled-components";
 
  const GlobalStyle = createGlobalStyle`
- /*font-family: 'Recursive', sans-serif; */
- /*font-family: 'Righteous', cursive;*/
+
     html, body, div, span, applet, object, iframe,
     h1, h2, h3, h4, h5, h6, p, blockquote, pre,
     a, abbr, acronym, address, big, cite, code,
